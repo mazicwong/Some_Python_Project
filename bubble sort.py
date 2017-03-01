b9296e61f532a8ca8f22b89a11e39053b1c8feb3
mazicwong <mazicwong@gmail.com>
2017-03-01 16:25:12 +0800

    so cool writing it

--- bubble sort.py
+++ bubble sort.py
@@ -8,5 +8,6 @@ for i in range(len(array) - 1, 0, -1):
     for j in range(0, i):
         if array[j] > array[j + 1]:
             array[j], array[j + 1] = array[j + 1], array[j]
+            # so cool compared to CPP...
 
 print(array)
