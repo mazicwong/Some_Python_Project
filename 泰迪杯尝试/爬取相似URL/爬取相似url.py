cb95b0c47ed10d37232da35092995d50b9ff72b9
mazicwong <mazicwong@gmail.com>
2017-04-01 23:53:33 +0800

    正确率 52.7%  有待改进

--- 泰迪杯尝试/爬取相似URL/爬取相似url.py
+++ 泰迪杯尝试/爬取相似URL/爬取相似url.py
@@ -6,22 +6,9 @@
 
 import urllib.request
 import re
+import os
 from bs4 import BeautifulSoup
 
-
-# 匹配该url是否相似
-def is_match(url, new_url):
-    len1 = len(url)
-    len2 = len(new_url)
-    if len1 != len2:
-        return False
-    for i in range(0, len1):
-        if url[i].islower():
-            if url[i]==new_url[i]:
-                continue
-            else:
-                return
-        elif
 
 # 按顺序放入txt
 def saveFile(data, cnt):
@@ -54,8 +41,8 @@ def getHtml(url, cnt):
         saveFail(url, cnt)
 
 
-def getSimilarHtml(url, cnt):
-    # 以下:get主页url
+# 以下:获得主页url   http://www.baidu.com/abc/cc  ==>>  www.baidu.com
+def get_root_url(url):
     m = url.split('//')
     if len(m) == 2:
         root_url = m[1]
@@ -63,26 +50,44 @@ def getSimilarHtml(url, cnt):
         root_url = m[0]
     tt = root_url.split('/')
     root_url = tt[0]
+    root_url = r'http://' + root_url
+    return root_url
 
-    # 在主页下面get新的URL,存入List
+
+# 获得主页html
+def get_root_html(url):
+    # 以下:get主页url   http://www.baidu.com/abc/cc  ==>>  www.baidu.com
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     }
-    req = urllib.request.Request(url=root_url, headers=headers)
-    response = urllib.request.urlopen(req, timeout=2)
-    html = response.read()
-    p1 = r''
-
-    # 判断是否相似URL
-    if is_match(url, new_url) == True:
-        saveFile(new, new_url)
-
+    try:
+        req = urllib.request.Request(url=url, headers=headers)
+        response = urllib.request.urlopen(req, timeout=4)
+        html = response.read()
+        return html
+    except:
+        print("出错了")
 
 
-
-        # p1 = r"" #正则表达式
-        # pat = re.compile(p1) #编译正则表达式
-        # re.find(pat,new_url)
+# 传入url，返回匹配的re
+def get_re(url):
+    url = url[7:]  # 去除http://
+    Len = len(url)
+    p = "http://"  # 这里不能用r,因为在下面str->bytes的时候\d会变成\\d
+    i = 0
+    while i < Len:
+        if url[i] == '.':
+            p += '.'
+        elif 'a' <= url[i] <= 'z':  # 不能直接判isplpha，因为str[i]中全都是字符
+            p += '[a-z]'
+        elif 'A' <= url[i] <= 'Z':  #http://www.shdxlt.cn/ShowPost.asp?ThreadID=144952
+            p += '[A-Z]'
+        elif '0' <= url[i] <= 'z':
+            p += '\d'
+        else:
+            p += url[i]
+        i += 1
+    return p
 
 
 # def getSimilarHtml(url, cnt):
@@ -114,9 +119,27 @@ def getUrl():
         urlList = file.readlines()
         cnt = 1
         for url in urlList:
-            getHtml(url, cnt)
-            getSimilarHtml(url, cnt)
-            cnt += 1
+            path = r'E:\泰迪杯\C题样例数据\All_html 相似url\爬取结果\out%s.txt' % cnt  # 用来判断文件是否已经存在
+            if os.path.isfile(path):  # 存在且不为空就退出
+                # if os.stat("out%s.txt" % cnt).st_size == 0
+                cnt += 1
+            else:
+                root_url = get_root_url(url)  # 获得主页URL
+                root_html = get_root_html(root_url)  # 获得主页html
+                p1 = get_re(url)  # 获取正则表达式
+                p1 = p1.encode(encoding='utf-8')  # str => bites (for the use of re.compile)
+                p1 = p1[:-1]  # 去掉换行符
+                pat = re.compile(p1)  # 编译正则表达式
+                List = re.findall(pat, root_html)
+                print("第%s个:%s个结果" % (cnt, len(List)))
+                path = r"E:\泰迪杯\C题样例数据\All_html 相似url\爬取结果\out%s.txt" % cnt
+                with open(path, "w") as f:
+                    for i in List:
+                        i = i.decode()  # 把b''decode掉，bytes=>string
+                        i = str(i)
+                        f.write(i)
+                        f.write('\n')
+                cnt += 1
 
 
 def main():
